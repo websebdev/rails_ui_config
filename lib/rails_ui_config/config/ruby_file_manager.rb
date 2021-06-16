3ad0141275843c1a8a0c6df75369640e44c119ca
Sebastien Auriault <sebastienauriault@gmail.com>
2021-06-15 20:29:45 -0400

    Refactor new line and option index in file manager and remove useless article views

--- lib/rails_ui_config/config/ruby_file_manager.rb
+++ lib/rails_ui_config/config/ruby_file_manager.rb
@@ -15,18 +15,18 @@ module RailsUiConfig
       end
 
       def update_option(name, value)
-        index = lines.find_index {|l| l.split("=").first.strip == "config.#{name}"}
+        index = option_index(name)
 
         if index # if the config is already there, replace it
           if value.blank?
             lines.delete_at(index)
           else
             indentation = calculate_indentation(lines[index])
-            lines[index] = "#{indentation}config.#{name} = #{value}\n"
+            lines[index] = new_line(indentation, name, value)
           end
         elsif value.present? # if not, just add it at the bottom
           indentation = calculate_indentation(lines[end_index - 1])
-          lines.insert(end_index, "#{indentation}config.#{name} = #{value}\n")
+          lines.insert(end_index, new_line(indentation, name, value))
         end
       end
 
@@ -36,8 +36,16 @@ module RailsUiConfig
 
       private
 
+      def new_line(indentation, name, value)
+        "#{indentation}config.#{name} = #{value}\n"
+      end
+
       def calculate_indentation(line)
         " " * line[/\A */].size
+      end
+
+      def option_index(option_name)
+        lines.find_index {|l| l.split("=").first.strip == "config.#{option_name}"}
       end
 
       def end_index
